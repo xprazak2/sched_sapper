27465d8b0b50f3f07154045199cbe0d6769fb253
Ondrej Prazak <oprazak@redhat.com>
2020-04-10 15:55:51 +0200

    First page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,46 +1,12 @@
 <style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
-
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
-
-	figure {
-		margin: 0 0 1em 0;
-	}
-
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
-
-	p {
-		margin: 1em auto;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
 </style>
 
 <svelte:head>
-	<title>Sapper project template</title>
+	<title>Sapper Sched</title>
 </svelte:head>
 
-<h1>Great success!</h1>
-
-<figure>
-	<img alt='Borat' src='great-success.png'>
-	<figcaption>HIGH FIVE!</figcaption>
-</figure>
-
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading. Jesus fucking Christ</strong></p>
+<div class="row">
+	<div class="column column-50 column-offset-25">
+		<h1>This is home page of Sapper Sched!</h1>
+	</div>
+</div>
---
+++ src/routes/users/index.svelte
@@ -0,0 +1,35 @@
+<svelte:head>
+  <title>Users</title>
+</svelte:head>
+
+<script context="module">
+  export function preload({ params, query }) {
+    return this.fetch('http://localhost:8080/users').then(res => res.json()).then(json => ({ users: json.users }));
+  }
+</script>
+
+<script>
+  export let users = [];
+
+  const fullName = (user) => `${user.name} ${user.surname}`;
+
+</script>
+
+<table>
+  <thead>
+    <tr>
+      <th>Name</th>
+      <th>Surname</th>
+      <th>Email</th>
+    </tr>
+  </thead>
+  <tbody>
+    {#each users as user (user.id) }
+    <tr>
+      <td>{user.name}</td>
+      <td>{user.surname}</td>
+      <td>{user.email}</td>
+    </tr>
+    {/each}
+  </tbody>
+</table>
